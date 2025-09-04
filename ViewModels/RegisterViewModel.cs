2ea3f0fe601723df5bcfea4d373d5227e11c9000
panosrodo <brandonroots@hotmail.gr>
2025-09-04 10:15:58 +0300

    feat: RegisterViewModel and Register View

---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookCatalog.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Όνομα χρήστη")]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Κωδικός")]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Επιβεβαίωση Κωδικού")]
+        [Compare("Password", ErrorMessage = "Οι κωδικοί δεν ταιριάζουν.")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
