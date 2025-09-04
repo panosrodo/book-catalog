e9a0e45719e2c7285c435839667289ea8ca064ea
panosrodo <brandonroots@hotmail.gr>
2025-09-04 08:09:16 +0300

    feat: add Book entity

---
+++ Data/Book.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookCatalog.Data
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        public string Author { get; set; } = string.Empty;
+
+        public int Year { get; set; }
+        public string ISBN { get; set; } = string.Empty;
+    }
+}
