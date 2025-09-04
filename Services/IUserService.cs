83f55678c07729deb40b3ad8a25348c230c45793
panosrodo <brandonroots@hotmail.gr>
2025-09-04 08:43:33 +0300

    feat: add IUserService interface with nullable Get methods

---
+++ Services/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using BookCatalog.Data;
+using BookCatalog.Models;
+using System.Collections.Generic;
+
+namespace BookCatalog.Services
+{
+    public interface IUserService
+    {
+        IEnumerable<User> GetAllUsers();
+        User? GetUserById(int id);
+        User? GetUserByUsername(string username);
+        void AddUser(User user);
+        void UpdateUser(User user);
+        void DeleteUser(int id);
+    }
+}
