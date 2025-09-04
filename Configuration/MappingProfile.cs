d02e504f20a588e3a8be38ace91289f2f3dd7ff7
panosrodo <brandonroots@hotmail.gr>
2025-09-04 09:14:59 +0300

    feat: add AutoMapper profile for Book and User mappings

---
+++ Configuration/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using BookCatalog.Data;
+using BookCatalog.DTOs;
+using BookCatalog.Models;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Book, BookDTO>().ReverseMap();
+        CreateMap<User, UserDTO>().ReverseMap();
+    }
+}
