ecd59a6da3fec299e549f8fe98f716bd4e4380f1
panosrodo <brandonroots@hotmail.gr>
2025-09-04 09:00:10 +0300

    feat: add UserController for user management with CRUD actions

---
+++ Controllers/UserController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BookCatalog.Data;
+using BookCatalog.Services;
+
+namespace BookCatalog.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        // GET: /User
+        public IActionResult Index()
+        {
+            var users = _userService.GetAllUsers();
+            return View(users);
+        }
+
+        // GET: /User/Details/5
+        public IActionResult Details(int id)
+        {
+            var user = _userService.GetUserById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // GET: /User/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /User/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _userService.AddUser(user);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // GET: /User/Edit/5
+        public IActionResult Edit(int id)
+        {
+            var user = _userService.GetUserById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // POST: /User/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _userService.UpdateUser(user);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // GET: /User/Delete/5
+        public IActionResult Delete(int id)
+        {
+            var user = _userService.GetUserById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // POST: /User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _userService.DeleteUser(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
