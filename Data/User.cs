aebab2807bf25f6da0ac6d9a17d3b610168ec5a0
panosrodo <brandonroots@hotmail.gr>
2025-09-04 08:08:59 +0300

    feat: add User entity

---
+++ Data/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookCatalog.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+
+        public string Role { get; set; } = string.Empty; // "Admin" ή "User"
+    }
+}
