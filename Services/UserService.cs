a094b014f91429bb98f6ad3ac0f2198f9c19f55b
panosrodo <brandonroots@hotmail.gr>
2025-09-04 08:43:47 +0300

    feat: add UserService implementation for user operations

---
+++ Services/UserService.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BookCatalog.Data;
+using BookCatalog.Models;
+
+namespace BookCatalog.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly BookCatalogDBContext _context;
+
+        public UserService(BookCatalogDBContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        public User? GetUserById(int id)
+        {
+            return _context.Users.Find(id);
+        }
+
+        public User? GetUserByUsername(string username)
+        {
+            return _context.Users.FirstOrDefault(u => u.Username == username);
+        }
+
+        public void AddUser(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+        }
+
+        public void UpdateUser(User user)
+        {
+            _context.Users.Update(user);
+            _context.SaveChanges();
+        }
+
+        public void DeleteUser(int id)
+        {
+            var user = _context.Users.Find(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
