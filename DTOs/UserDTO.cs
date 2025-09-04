7d026317ce082bbc42ee41804082298e297c0c66
panosrodo <brandonroots@hotmail.gr>
2025-09-04 08:39:59 +0300

    feat: add UserDTO class for user transfer object

---
+++ DTOs/UserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BookCatalog.DTOs
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
+}
