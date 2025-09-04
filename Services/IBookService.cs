04feb395cb93f45489fd8603b060b141537ffb58
panosrodo <brandonroots@hotmail.gr>
2025-09-04 08:42:54 +0300

    feat: add IBookService interface with nullable Get method

---
+++ Services/IBookService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using BookCatalog.DTOs;
+
+namespace BookCatalog.Services
+{
+    public interface IBookService
+    {
+        IEnumerable<BookDTO> GetAll();
+        BookDTO? Get(int id);
+        void Add(BookDTO book);
+        void Update(BookDTO book);
+        void Delete(int id);
+    }
+}
