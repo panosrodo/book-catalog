51691695ddef8b03e753f6771fd287152f72e84b
panosrodo <brandonroots@hotmail.gr>
2025-09-04 08:09:54 +0300

    feat: add initial DbContext configuration

---
+++ Data/BookCatalogDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookCatalog.Data
+{
+    public class BookCatalogDBContext : DbContext
+    {
+        public BookCatalogDBContext(DbContextOptions<BookCatalogDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
