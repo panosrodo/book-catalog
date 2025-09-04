806991d1e4e8bcc65ba6b34fd8a1da09169fb590
panosrodo <brandonroots@hotmail.gr>
2025-09-04 08:30:32 +0300

    feat: add IBookRepository interface

---
+++ Repositories/IBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using BookCatalog.Data;
+using BookCatalog.Models;
+using System.Collections.Generic;
+
+namespace BookCatalog.Repositories
+{
+    public interface IBookRepository
+    {
+        IEnumerable<Book> GetAll();
+        Book? Get(int id);
+        void Add(Book book);
+        void Update(Book book);
+        void Delete(int id);
+    }
+}
