9742c5a603dcb1e8ecbad37114edc738d3f426c9
panosrodo <brandonroots@hotmail.gr>
2025-09-04 09:18:46 +0300

    refactor: use AutoMapper and constructor injection in BookService"

--- Services/BookService.cs
+++ Services/BookService.cs
@@ -1,64 +1,42 @@
-﻿using BookCatalog.Data;
+﻿using AutoMapper;
+using BookCatalog.Data;
 using BookCatalog.DTOs;
 using BookCatalog.Models;
 using BookCatalog.Repositories;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace BookCatalog.Services
 {
     public class BookService : IBookService
     {
         private readonly IBookRepository _repository;
-        public BookService(IBookRepository repository)
+        private readonly IMapper _mapper;
+
+        public BookService(IBookRepository repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
         public IEnumerable<BookDTO> GetAll() =>
-            _repository.GetAll().Select(b => new BookDTO
-            {
-                Id = b.Id,
-                Title = b.Title,
-                Author = b.Author,
-                Year = b.Year,
-                ISBN = b.ISBN
-            });
+            _mapper.Map<IEnumerable<BookDTO>>(_repository.GetAll());
 
         public BookDTO? Get(int id)
         {
             var b = _repository.Get(id);
-            return b == null ? null : new BookDTO
-            {
-                Id = b.Id,
-                Title = b.Title,
-                Author = b.Author,
-                Year = b.Year,
-                ISBN = b.ISBN
-            };
+            return b == null ? null : _mapper.Map<BookDTO>(b);
         }
 
         public void Add(BookDTO book)
         {
-            _repository.Add(new Book
-            {
-                Title = book.Title,
-                Author = book.Author,
-                Year = book.Year,
-                ISBN = book.ISBN
-            });
+            var entity = _mapper.Map<Book>(book);
+            _repository.Add(entity);
         }
 
         public void Update(BookDTO book)
         {
-            _repository.Update(new Book
-            {
-                Id = book.Id,
-                Title = book.Title,
-                Author = book.Author,
-                Year = book.Year,
-                ISBN = book.ISBN
-            });
+            var entity = _mapper.Map<Book>(book);
+            _repository.Update(entity);
         }
 
         public void Delete(int id) => _repository.Delete(id);
