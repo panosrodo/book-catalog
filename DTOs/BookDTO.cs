aee1db586e0a38c843bc3b2b134e0c1ba2711047
panosrodo <brandonroots@hotmail.gr>
2025-09-04 08:39:45 +0300

    feat: add BookDTO class for book transfer object

---
+++ DTOs/BookDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace BookCatalog.DTOs
+{
+    public class BookDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public string ISBN { get; set; } = string.Empty;
+    }
+}
