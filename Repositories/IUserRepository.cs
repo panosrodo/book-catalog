931fb70256b0e6412dff9db4406fceeed3c2c8e5
panosrodo <brandonroots@hotmail.gr>
2025-09-04 08:31:40 +0300

    feat: add IUserRepository interface

---
+++ Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using BookCatalog.Data;
+
+public interface IUserRepository
+{
+    IEnumerable<User> GetAll();
+    User? GetById(int id);
+    User? GetByUsername(string username);
+    void Add(User user);
+    void Update(User user);
+    void Delete(int id);
+}
